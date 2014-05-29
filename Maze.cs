e391e20b403ec192ac1faf6b489f76b96239813d
Zdravko Danev <zdanev@gmail.com>
2014-05-28 23:13:32 -0700

    initial commit

---
+++ Maze.cs
@@ -0,0 +1,85 @@
+﻿namespace MazeKata
+{
+    using System.Collections.Generic;
+
+    public class Maze
+    {
+        private const int wall = -1;
+        private const int empty = 0;
+
+        public int Width { get; private set; }
+
+        public int Height { get; private set; }
+
+        private int[,] cells;
+
+        public Maze(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            cells = new int[height, width];
+        }
+
+        public bool IsEmpty(int x, int y)
+        {
+            return cells[x, y] == empty;
+        }
+
+        public bool IsWall(int x, int y)
+        {
+            return cells[x, y] == wall;
+        }
+
+        public void SetWall(int x, int y)
+        {
+            cells[x, y] = wall;
+        }
+
+        public void SetValue(int x, int y, int value)
+        {
+            cells[x, y] = value;
+        }
+
+        public int GetValue(int x, int y)
+        {
+            return cells[x, y];
+        }
+
+        public bool AreConnected(int x1, int y1, int x2, int y2)
+        {
+            return Distance(x1, y1, x2, y2) > 0;
+        }
+
+        public int Distance(int x1, int y1, int x2, int y2)
+        {
+            Wave(x1, y1);
+            return cells[x2, y2];
+        }
+
+        private Queue<Point> queue = new Queue<Point>();
+
+        private void Wave(int x, int y)
+        {
+            GoTo(x, y, 1);
+
+            while (queue.Count > 0)
+            {
+                Point p = queue.Dequeue();
+                int dist = GetValue(p.X, p.Y) + 1;
+                GoTo(p.X - 1, p.Y, dist);
+                GoTo(p.X + 1, p.Y, dist);
+                GoTo(p.X, p.Y - 1, dist);
+                GoTo(p.X, p.Y + 1, dist);
+            }
+        }
+
+        private void GoTo(int x, int y, int dist)
+        {
+            if (0 <= x && x < Width && 0 <= y && y < Height && IsEmpty(x, y))
+            {
+                SetValue(x, y, dist);
+                queue.Enqueue(new Point(x, y));
+            }
+        }
+    }
+}
---
+++ MazeTests.cs
@@ -0,0 +1,61 @@
+﻿namespace MazeKata
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MazeTests
+    {
+        // 1 2 3
+        // 2 3 4
+        // 3 4 5
+        [TestMethod]
+        public void EmptyMaze()
+        {
+            // arrange
+            var maze = new Maze(3, 3);
+
+            // act
+            var dist = maze.Distance(0, 0, 2, 2);
+
+            // assert
+            Assert.AreEqual(5, dist);
+        }
+
+        // 1 2 w
+        // 2 w _
+        // w _ _
+        [TestMethod]
+        public void Disconnected()
+        {
+            // arrange
+            var maze = new Maze(3, 3);
+            maze.SetWall(2, 0);
+            maze.SetWall(1, 1);
+            maze.SetWall(0, 2);
+
+            // act
+            var connected = maze.AreConnected(0, 0, 2, 2);
+
+            // assert
+            Assert.AreEqual(false, connected);
+        }
+
+        // 1 w 7 
+        // 2 w 6
+        // 3 4 5
+        [TestMethod]
+        public void FindWay()
+        {
+            // arrange
+            var maze = new Maze(3, 3);
+            maze.SetWall(1, 0);
+            maze.SetWall(1, 1);
+
+            // act
+            var dist = maze.Distance(0, 0, 2, 0);
+
+            // assert
+            Assert.AreEqual(7, dist);
+        }
+    }
+}
